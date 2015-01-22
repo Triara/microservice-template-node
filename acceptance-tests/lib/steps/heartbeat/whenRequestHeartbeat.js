26d6466c744260c648990daa6783cd1ff92fa5c9
Kristina <kristina.lyubomirova@bbva.com>
2015-01-22 12:29:42 +0100

    tests: Use lodash bind

--- acceptance-tests/lib/steps/heartbeat/whenRequestHeartbeat.js
+++ acceptance-tests/lib/steps/heartbeat/whenRequestHeartbeat.js
@@ -1,26 +1,28 @@
 'use strict';
 
-const request = require('request'),
-    config = JSON.parse(require('fs').readFileSync('./config.json', 'utf8')),
+const config = JSON.parse(require('fs').readFileSync('./config.json', 'utf8')),
     World = require('../world'),
     should = require('chai').should(),
+    _ = require('lodash'),
     expect = require('chai').expect;
+let request = require('request');
 
 module.exports = whenRequestHeartbeatStep;
 
 function whenRequestHeartbeatStep() {
     this.When(/^I request the heartbeat$/, function (done) {
-        var OPTIONS = {
+        const OPTIONS = {
             url: 'http://localhost:' + config.publicPort + '/heartbeat',
             method: 'GET'
         };
-        var world = this;
 
-        request(OPTIONS, function(err, res, body){
+        let callback = function(err, res, body){
             expect(err).to.be.null;
 
-            world.publishValue('statusCode', res.statusCode);
+            this.publishValue('statusCode', res.statusCode);
             done();
-        });
+        };
+
+        request(OPTIONS, _.bind(callback, this));
     });
 }
